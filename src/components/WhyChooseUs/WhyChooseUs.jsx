27298fa31ae8ed99fd70c0edea55bea50a8b59b7
AaryanSwami <swamiaaryan17@gmail.com>
2025-05-21 20:11:19 +0530

    fixed Why Choose Us

--- src/components/WhyChooseUs/WhyChooseUs.jsx
+++ src/components/WhyChooseUs/WhyChooseUs.jsx
@@ -122,7 +122,7 @@ const WhyChooseUs = () => {
               />
             </div>
             <motion.div
-              className="h-5/6 lg:my-auto my-2"
+              className="h-5/6 md:my-auto "
               initial={{ scale: 0.8, opacity: 0 }}
               whileInView={{ scale: 1, opacity: 1 }}
               transition={{ duration: 0.6, delay: 1.2 }}
